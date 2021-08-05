3bc62187c1b934d610fddbe364c80e6a95b84f9a
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-08-05 11:06:54 +0800

    Game Launcher

---
+++ launcher.bat
@@ -0,0 +1,25 @@
+@echo off
+title Cmd Game Launcher
+if exist "%USERPROFILE%\cgl\games" (cd %USERPROFILE%\cgl\ & goto nogame) else (goto ask)
+
+:ask
+set /p ans="What Do You Want To Do?(Launch A Game=l/Download A Game=dg):"
+if %ans%=l (goto launch)
+if %ans%=dg (start %USERPROFILE%\cgl\ajgamestore.bat & exit)
+echo Please type l or dg.
+goto ask
+
+:launch
+cls
+type "%USERPROFILE%\cgl\list.cfg"
+set /p launch="Type A Game To Launch:"
+start "%USERPROFILE%\cgl\games\%launch%\%launch%.bat"
+exit
+
+:nogame
+echo You Don't Have Games.
+echo Press Any Key To Start Store.
+pause > nul
+echo List Of Games >> list.cfg & md games & md saves
+start %USERPROFILE%\cgl\ajgamestore.bat
+exit
